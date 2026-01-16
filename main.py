84450d1788a8ac0da5e0d47030f7a2c091d95fb3
Giorgiod91 <giorgio.dettmar@gmx.de>
2026-01-16 20:08:00 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+class Task(self,name, weekly_hours, deadline_day, priority):
+    self.name = name
+    self.weekly_hours = weekly_hours
+    self.deadline_day =deadline_day
+    self.priority = priority
+
+
